206e48e8628a460a765300cc41e792c6676e4296
kharisma-ky <hiwottlhn9@gmail.com>
2024-05-21 19:43:26 -0700

    SQL

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,8 @@
 -- prints the full description of the table
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
+
 SELECT column_name, column_type, is_nullable, column_key, column_default, extra
 FROM information_schema.columns
 WHERE table_name = 'first_table';
